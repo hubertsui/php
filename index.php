47406cd12ffc3b9a6f65c9c8899ef78991beab42
Hubert Sui <huberts@canviz.com>
2018-03-14 17:09:29 +0800

    display errors

--- index.php
+++ index.php
@@ -1,11 +1,14 @@
 <?php
+ini_set('display_errors',1);
+ini_set('display_startup_errors',1);
+error_reporting(-1);
+
 require_once 'HelloWorld.php';
 $myfile = fopen("D:\home\data\mysql\MYSQLCONNSTR_localdb.ini", "r") or die("Unable to open file!");
 $connStr = fread($myfile,filesize("D:\home\data\mysql\MYSQLCONNSTR_localdb.ini"));
 fclose($myfile);
 echo $connStr;
 $pdo = new PDO($connStr);
-$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $pdo->query("CREATE TABLE hello (what VARCHAR(50) NOT NULL)");
 $hw=new HelloWorld($pdo);
 $content=$hw->hello();
