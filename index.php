a489d0ec7b4bd81e3fd7619047cfcac4e7e976ec
Hubert Sui <huberts@canviz.com>
2018-03-14 22:12:06 +0800

    change ini to txt

--- index.php
+++ index.php
@@ -4,8 +4,8 @@ ini_set('display_startup_errors',1);
 error_reporting(-1);
 
 require_once 'HelloWorld.php';
-$myfile = fopen("D:\home\data\mysql\MYSQLCONNSTR_localdb.ini", "r") or die("Unable to open file!");
-$conns = explode(";",fread($myfile,filesize("D:\home\data\mysql\MYSQLCONNSTR_localdb.ini")));
+$myfile = fopen("D:\home\data\mysql\MYSQLCONNSTR_localdb.txt", "r") or die("Unable to open file!");
+$conns = explode(";",fread($myfile,filesize("D:\home\data\mysql\MYSQLCONNSTR_localdb.txt")));
 fclose($myfile);
 echo implode(" ",$conns);
 $pdo = new PDO("mysql:dbname=" . explode("=",$conns[0])[1] . ";host=" . explode("=",$conns[1])[1], explode("=",$conns[2])[1], explode("=",$conns[3])[1]);
