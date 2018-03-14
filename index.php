3f39c1319879a2414a10ffd45507b0d1498a11bb
Hubert Sui <huberts@canviz.com>
2018-03-14 22:07:16 +0800

    change

--- index.php
+++ index.php
@@ -5,10 +5,10 @@ error_reporting(-1);
 
 require_once 'HelloWorld.php';
 $myfile = fopen("D:\home\data\mysql\MYSQLCONNSTR_localdb.ini", "r") or die("Unable to open file!");
-$conns = explode("[=;]",fread($myfile,filesize("D:\home\data\mysql\MYSQLCONNSTR_localdb.ini")));
+$conns = explode(";",fread($myfile,filesize("D:\home\data\mysql\MYSQLCONNSTR_localdb.ini")));
 fclose($myfile);
 echo implode(" ",$conns);
-$pdo = new PDO("mysql:dbname=" . $conns[1] . ";host=" . $conns[3], $conns[5], $conns[7]);
+$pdo = new PDO("mysql:dbname=" . explode("=",$conns[0])[1] . ";host=" . explode("=",$conns[1])[1], explode("=",$conns[2])[1], explode("=",$conns[3])[1]);
 $pdo->query("CREATE TABLE hello (what VARCHAR(50) NOT NULL)");
 $hw=new HelloWorld($pdo);
 $content=$hw->hello();
