79dda36ed97f9f116ef56ade8c9efaf295650db4
Hubert Sui <huberts@canviz.com>
2018-03-14 18:33:09 +0800

    fix bugs with conns

--- index.php
+++ index.php
@@ -5,10 +5,10 @@ error_reporting(-1);
 
 require_once 'HelloWorld.php';
 $myfile = fopen("D:\home\data\mysql\MYSQLCONNSTR_localdb.ini", "r") or die("Unable to open file!");
-$conns = strtok(fread($myfile,filesize("D:\home\data\mysql\MYSQLCONNSTR_localdb.ini")),"=;");
+$conns = split("[=;]",fread($myfile,filesize("D:\home\data\mysql\MYSQLCONNSTR_localdb.ini")));
 fclose($myfile);
 echo implode(" ",$conns);
-$pdo = new PDO("mysql:dbname=" . $conns[1] . ";host=" . $conns[3], $conn[5], $conn[7]);
+$pdo = new PDO("mysql:dbname=" . $conns[1] . ";host=" . $conns[3], $conns[5], $conns[7]);
 $pdo->query("CREATE TABLE hello (what VARCHAR(50) NOT NULL)");
 $hw=new HelloWorld($pdo);
 $content=$hw->hello();
