35feac6449f9866ac956853caaf8440200d6d1a0
Hubert Sui <huberts@canviz.com>
2018-03-14 16:59:45 +0800

    change connstr

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <?php
 require_once 'HelloWorld.php';
-$pdo = new PDO(getenv("MYSQLCONNSTR_localdb"));
+$myfile = fopen("D:\home\data\mysql\MYSQLCONNSTR_localdb.ini", "r") or die("Unable to open file!");
+$connStr = fread($myfile,filesize("D:\home\data\mysql\MYSQLCONNSTR_localdb.ini"));
+fclose($myfile);
+echo $connStr;
+$pdo = new PDO($connStr);
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $pdo->query("CREATE TABLE hello (what VARCHAR(50) NOT NULL)");
 $hw=new HelloWorld($pdo);
