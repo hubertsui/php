23573b9c1f0f298bb331a3db2a8d31d1c1981b14
Hubert Sui <huberts@canviz.com>
2018-03-16 14:46:41 +0800

    test 2

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,3 +4,8 @@ echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO 
 sudo apt-key adv --keyserver packages.microsoft.com --recv-keys 52E16F86FEE04B979B07E28DB02C46DF417A0893
 sudo apt-get install apt-transport-https
 sudo apt-get update && sudo apt-get install azure-cli
+echo $AZURE_USERNAME
+echo $AZURE_PASSWORD
+echo $AZURE_TENANT_ID
+az login --service-principal -u $AZURE_USERNAME -p $AZURE_PASSWORD --tenant $AZURE_TENANT_ID
+az container create --resource-group MSAzure-ACIAKS-CICD-Demo --name CICD-Demo --image microsoft/aci-helloworld --dns-name-label aci-cicd-demo --ports 80
